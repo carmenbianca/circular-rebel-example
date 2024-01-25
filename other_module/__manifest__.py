21fee3b3cf433567096313e02bdd301df06a7232
Carmen Bianca BAKKER <carmen@carmenbianca.eu>
2024-01-25 17:14:21 +0100

    initial commit
    Signed-off-by: Carmen Bianca BAKKER <carmen@carmenbianca.eu>

---
+++ other_module/__manifest__.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "other_module",
+    "summary": """
+        other example""",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/circular-rebel-example",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["spp_pos_mustard"],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
